3bbae1296fa26453af2ea5084cd82dc5d66632e0
Thanatuch Lertritsirikul <tar@Tars-MacBook-Pro.local>
2023-11-25 14:21:02 +0700

    remove comment

--- mongo-init.js
+++ mongo-init.js
@@ -1,12 +1,2 @@
 db = db.getSiblingDB('giant_bakery');
 db.createCollection('customer');
-
-// db = db.getSiblingDB('admin');
-// db.createUser({
-//     user: "root",
-//     pwd: "123456",
-//     roles: [
-//         { role: "root", db: "admin" },
-//         { role: "readWrite", db: "giant-bakery" }
-//     ]
-// });
